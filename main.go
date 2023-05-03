8d4fad135abe02092f654de613d3362667f11406
James Kehan <jxk5891@psu.edu>
2023-05-03 09:50:52 -0400

    Personalized to our project

--- main.go
+++ main.go
@@ -8,42 +8,37 @@ import (
 )
 
 func main() {
-
 	// Encoding function is defined
 	encode := enigma.NewEnigma(
-
-		// Rotors are defined
+		// Rotors are defined with established starting points and go through rotation of alphabet
 		[]enigma.RotorConfig{
-			enigma.RotorConfig{ID: "III", Start: 'A', Ring: 1},
-			enigma.RotorConfig{ID: "II", Start: 'B', Ring: 1},
-			enigma.RotorConfig{ID: "IV", Start: 'C', Ring: 1},
+			enigma.RotorConfig{ID: "III", Start: 'I', Ring: 1},
+			enigma.RotorConfig{ID: "II", Start: 'S', Ring: 1},
+			enigma.RotorConfig{ID: "IV", Start: 'T', Ring: 1},
 		},
-
 		// Plugboard establishes the specific change in letters
 		"B",
 		[]string{"AB", "CD", "EF"},
 	)
-
 	// Encoding function is used
 	plaintext := "LMSIX"
+
 	ciphertext := encode.EncodeString(plaintext)
 
 	fmt.Printf("%s is encoded as %s\n", plaintext, ciphertext)
 
 	decode := enigma.NewEnigma(
+		// Rotors are defined same as encode and go through their rotation
 		[]enigma.RotorConfig{
-			enigma.RotorConfig{ID: "III", Start: 'A', Ring: 1},
-			enigma.RotorConfig{ID: "II", Start: 'B', Ring: 1},
-			enigma.RotorConfig{ID: "IV", Start: 'C', Ring: 1},
+			enigma.RotorConfig{ID: "III", Start: 'I', Ring: 1},
+			enigma.RotorConfig{ID: "II", Start: 'S', Ring: 1},
+			enigma.RotorConfig{ID: "IV", Start: 'T', Ring: 1},
 		},
-
 		// Plugboard changes letters back
 		"B",
 		[]string{"AB", "CD", "EF"},
 	)
+	decoded := decode.EncodeString(ciphertext)
 
-	plaintext2 := ciphertext
-	ciphertext2 := decode.EncodeString(plaintext2)
-
-	fmt.Printf("%s is encoded as %s\n", ciphertext, ciphertext2)
+	fmt.Printf("%s is encoded as %s\n", ciphertext, decoded)
 }
